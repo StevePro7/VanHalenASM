ddf48ed5194f5e409681572e344ba4f2eff4e011
StevePro7 <steveprozune@hotmail.com>
2021-02-04 18:25:48 +0000

    no message

--- asm/01-Simple/main.asm
+++ asm/01-Simple/main.asm
@@ -43,70 +43,25 @@ main:
     ;==============================================================
     ; Load tiles (font)
     ;==============================================================
-    ; 1. Set VRAM write address to tile index 0
-    ; by outputting $4000 ORed with $0000
-    ld a,$00
-    out ($bf),a
-    ld a,$40
-    out ($bf),a
-    ; 2. Output tile data
-    ld hl,FontData              ; Location of tile data
-    ld bc,FontDataEnd-FontData  ; Counter for number of bytes to write
-    WriteTilesLoop:
-        ; Output data byte then three zeroes, because our tile data is 1 bit
-        ; and must be increased to 4 bit
-        ld a,(hl)        ; Get data byte
-        out ($be),a
-        inc hl           ; Add one to hl so it points to the next data byte
-        dec bc
-        ld a,b
-        or c
-        jp nz,WriteTilesLoop
+    call engine_vram_manager_load_tiles
+
 
     ;==============================================================
     ; Write text to name table
     ;==============================================================
-    ; 1. Set VRAM write address to name table index 0
-    ; by outputting $4000 ORed with $3800+0
-    ld a,$00
-    out ($bf),a
-    ld a,$38|$40
-    out ($bf),a
-    ; 2. Output tilemap data
-    ld hl,Message
-    ld bc,MessageEnd-Message  ; Counter for number of bytes to write
-    WriteTextLoop:
-        ld a,(hl)    ; Get data byte
-        out ($be),a
-        inc hl       ; Point to next letter
-        dec bc
-        ld a,b
-        or c
-        jp nz,WriteTextLoop
+    call engine_text_manager_write_text
+
 
     ; Turn screen on
-    ld a,%11000000
-;          |||| |`- Zoomed sprites -> 16x16 pixels
-;          |||| `-- Doubled sprites -> 2 tiles per sprite, 8x16
-;          |||`---- 30 row/240 line mode
-;          ||`----- 28 row/224 line mode
-;          |`------ VBlank interrupts
-;          `------- Enable display
-    out ($bf),a
-    ld a,$81
-    out ($bf),a
+    call engine_screen_manager_display_on
+    
 
     ; Infinite loop to stop program
 Loop:
      jp Loop
 
+
 ;==============================================================
 ; Data
 ;==============================================================
-
-
-
-
-
-
 .include "data.inc"
