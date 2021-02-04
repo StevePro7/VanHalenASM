c9d8f36d54e24d36cc7cbfbcba3c71888bc36274
StevePro7 <steveprozune@hotmail.com>
2021-02-04 19:56:32 +0000

    no message

--- asm/02-Smallest/main.asm
+++ asm/02-Smallest/main.asm
@@ -1,16 +1,8 @@
-.memorymap
-defaultslot 0
-slotsize $8000
-slot 0 $0000
-.endme
-
-.rombankmap
-bankstotal 1
-banksize $8000
-banks 1
-.endro
-
 .sdsctag 1.0,"Hello World","Simple Sega Master System demo to run on real hardware","StevePro Studios"
+
+.include "memory.inc"
+.include "variables.inc"
+
 
 .bank 0 slot 0
 .org $0000
@@ -29,7 +21,7 @@ banks 1
 ; Main program
 main:
     ld sp, $dff0
-
+    ld de, _RAM_FFFC_
     
     ; Infinite loop to stop program
 Loop:
