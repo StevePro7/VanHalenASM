342d71c179c74bc1b74731e1f676fa6c4f9cdfde
StevePro7 <steveprozune@hotmail.com>
2021-02-04 18:18:58 +0000

    no message

--- asm/01-Simple/main.asm
+++ asm/01-Simple/main.asm
@@ -25,46 +25,20 @@
 main:
     ld sp, $dff0
 
-    ;==============================================================
     ; Set up VDP registers
-    ;==============================================================
-    ld hl,VdpData
-    ld b,VdpDataEnd-VdpData
-    ld c,$bf
-    otir
+    call engine_vram_manager_load_vdpreg
 
     ;==============================================================
     ; Clear VRAM
     ;==============================================================
-    ; 1. Set VRAM write address to 0 by outputting $4000 ORed with $0000
-    ld a,$00
-    out ($bf),a
-    ld a,$40
-    out ($bf),a
-    ; 2. Output 16KB of zeroes
-    ld bc, $4000    ; Counter for 16KB of VRAM
-    ClearVRAMLoop:
-        ld a,$00    ; Value to write
-        out ($be),a ; Output to VRAM address, which is auto-incremented after each write
-        dec bc
-        ld a,b
-        or c
-        jp nz,ClearVRAMLoop
-
+    call engine_vram_manager_clear_vram
+    
+    
     ;==============================================================
     ; Load palette
     ;==============================================================
-    ; 1. Set VRAM write address to CRAM (palette) address 0 (for palette index 0)
-    ; by outputting $c000 ORed with $0000
-    ld a,$00
-    out ($bf),a
-    ld a,$c0
-    out ($bf),a
-    ; 2. Output colour data
-    ld hl,PaletteData
-    ld b,(PaletteDataEnd-PaletteData)
-    ld c,$be
-    otir
+    call engine_vram_manager_load_palette
+
 
     ;==============================================================
     ; Load tiles (font)
